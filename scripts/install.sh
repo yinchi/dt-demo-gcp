75e2747c292646ae04269033ec71295e38509f14
Yin-Chi Chan <ycc39@cam.ac.uk>
2025-08-10 07:11:59 +0100

    Update install.sh with Docker and K8s

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,23 +17,87 @@ if ! command -v gum &> /dev/null; then
     exit 1
 fi
 
-gum log "Select a package to install:"
-package=$(gum choose "quarto")
+gum log "Select a set to install:"
+package=$(gum choose "Docker" "Kubernetes")
 
 # Branch based on selected package
 case $package in
-    "quarto")
-        gum log "🛠️ Installing quarto..."
-        if ! (curl -fsSL https://quarto.org/download/latest/quarto-linux-amd64.deb -o quarto.deb && \
-              sudo dpkg -i quarto.deb && \
-              rm quarto.deb); then
-            gum log "❌ Failed to install quarto. Please check your internet connection or permissions."
-            exit 1
+    "Docker")
+        # Docker Engine -- do not prompt to update as it is a critical component
+        if command -v docker &> /dev/null; then
+            gum log "✅ Docker Engine is already installed."
+        else
+            gum log "🛠️  Installing Docker Engine..."
+            if ! sudo apt-get install -yqq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; then
+                gum log "❌ Failed to install Docker Engine."
+                exit 1
+            else
+                gum log "✅ Docker Engine installed successfully."
+            fi
         fi
-        gum log "✅ quarto installed successfully."
+        # Optional: Lazydocker
+        if ! gum confirm "Do you want to install/update Lazydocker?"; then
+            gum log " Skipping Lazydocker installation."
+        else
+            gum log "🛠️  Installing or updating Lazydocker..."
+            if ! curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash; then
+                gum log "❌ Failed to install Lazydocker."
+                exit 1
+            else
+                gum log "✅ Lazydocker installed successfully."
+            fi
+        fi
+        ;;
+    "Kubernetes")
+        # Minikube -- do not prompt to update as it is a critical component
+        if command -v minikube &> /dev/null; then
+            gum log "✅ Minikube is already installed."
+        else
+            gum log "🛠️  Installing Minikube..."
+            if ! (curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb \
+                && sudo dpkg -i minikube_latest_amd64.deb \
+                && rm minikube_latest_amd64.deb
+            ); then
+                gum log "❌ Failed to install Minikube."
+                exit 1
+            else
+                gum log "✅ Minikube installed successfully."
+            fi
+        fi
+        # Optional: K9s
+        if ! gum confirm "Do you want to install/update K9s?"; then
+            gum log "Skipping K9s installation."
+        else
+            gum log "🛠️  Installing or updating K9s..."
+            if ! curl -LO https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && sudo apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb; then
+                gum log "❌ Failed to install K9s."
+                exit 1
+            else
+                gum log "✅ K9s installed successfully."
+            fi
+        fi
+        # Optional: start minikube cluster
+        # TODO: figure out reasonable defaults. For now, default to "No" response.
+        if gum confirm "Do you want to start the \"minikube\" cluster?" --default=false; then
+            # Check if "minikube" cluster is running.  Note `minikube status -p minikube`
+            # returns a non-zero exit code if the cluster does not exist
+            if ! minikube -p minikube status -f {{.Host}} | grep -i Running &> /dev/null; then
+                gum log "🛠️  Starting minikube cluster..."
+                if ! minikube -p minikube start --interactive=true; then
+                    gum log "❌ Failed to start minikube cluster."
+                    exit 1
+                else
+                gum log "✅ Minikube cluster started successfully."
+                fi
+            else
+                gum log "❗ Minikube cluster is already running, \`start\` skipped."
+            fi
+        else
+            gum log "Skipping minikube cluster start."
+        fi
         ;;
     *)
-        gum log "❌ Unknown package: $package"
+        gum log "❌ Unknown software set: $package"
         exit 1
         ;;
 esac
