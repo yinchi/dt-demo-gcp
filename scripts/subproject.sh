e1a9cf86dd424a9054380de84f3fda6470352ea5
Yin-Chi Chan <ycc39@cam.ac.uk>
2025-08-10 09:56:46 +0100

    Update subproject.sh; add code to create Python project

--- scripts/subproject.sh
+++ scripts/subproject.sh
@@ -55,33 +55,70 @@ setup_python_mkdocs() {
     echo "Serve locally with: uv run mkdocs serve"
 }
 
-# Function to handle Python app subproject setup
-setup_python_app() {
-    echo "❌ Create script for 'Python app' not yet implemented"
-    exit 1
+# Function to handle Python project setup
+setup_python_project() {
+    echo "What is the name of the Python project?"
+    project_name=$(gum input --placeholder "Name")
+
+    # Check if the project name is empty
+    if [[ -z "$project_name" ]]; then
+        echo "❌ No project name provided, aborting."
+        exit 1
+    fi
+
+    # Check that `./$project_name` does not already exist
+    if [[ -d "./$project_name" ]]; then
+        echo "❌ Project '$project_name' already exists, aborting."
+        exit 1
+    fi
+
+    # --app, --package, or both?
+    echo "Select the type of Python project to create:"
+    app_type=$(gum choose "App" "Library")
+    app_flags=()
+    if [[ "$app_type" == "App" ]]; then
+        app_flags+=(--app --package)
+    elif [[ "$app_type" == "Library" ]]; then
+        app_flags+=(--lib)
+    fi
+
+    # Create the project. Note uv will automatically search for a workspace in a parent directory.
+    echo "Creating Python project: $project_name"
+    if ! uv init "${app_flags[@]}" "$project_name"; then
+        echo "❌ Failed to create Python project: $project_name"
+        exit 1
+    fi
+
+    # Search src/* and remove py.typed if found; `--lib` adds this but we won't use it
+    # (We can use type hints but won't enforce them)
+    # Use `find` as we don't know the exact location (e.g. foo-bar may become foo_bar)
+    find "$project_name/src/" -name "py.typed" -exec rm -f {} +
+
+    # Run uv sync to add the project to the workspace's virtual environment
+    uv sync --package "$project_name"
 }
 
-# Function to handle Node.js app subproject setup
-setup_nodejs_app() {
-    echo "❌Create script for 'Node.js app' not yet implemented"
+# Function to handle Node.js project setup
+setup_nodejs_project() {
+    echo "❌Create script for 'Node.js project' not yet implemented"
     exit 1
 }
 
 # Use gum to prompt for subproject type
 echo "Select the type of subproject to create:"
 subproject_type=$(gum choose "Python mkdocs"\
-                             "Python app"\
-                             "Node.js app")
+                             "Python project"\
+                             "Node.js project")
 
 case $subproject_type in
     "Python mkdocs")
         setup_python_mkdocs
         ;;
-    "Python app")
-        setup_python_app
+    "Python project")
+        setup_python_project
         ;;
-    "Node.js app")
-        setup_nodejs_app
+    "Node.js project")
+        setup_nodejs_project
         ;;
     *)
         echo "Invalid subproject type selected."
