08216a7afa70ecaf5d9ee7b3efe2fa8abaa2d86b
Yin-Chi Chan <ycc39@cam.ac.uk>
2025-08-12 09:36:30 +0100

    Initial FastAPI setup

--- dt-demo-gcp-auth/src/dt_demo_gcp/auth/__init__.py
+++ dt-demo-gcp-auth/src/dt_demo_gcp/auth/__init__.py
@@ -1,9 +1,55 @@
 """Authentication module for the DT demo."""
 
+import sys
 
-def main() -> None:
-    """Test function.
+import pydantic as pyd
+from fastapi import FastAPI, cli
+from fastapi.responses import PlainTextResponse
 
-    Print hello message when module is run via `uv run --package dt-demo-gcp-auth main`.
+from .__version__ import __version__ as version
+
+
+def plaintext_example(value: str, status_code: int = 200) -> dict[int, dict]:
+    """Generate the response schema with `value` as the example for a plaintext response."""
+    return {status_code: {"content": {"text/plain": {"example": value}}}}
+
+
+app = FastAPI()
+
+
+class Message(pyd.BaseModel):
+    """Message model.
+
+    Since HTTPException uses `detail` to convey error messages, we use the same field name here.
     """
-    print("Hello from dt-demo-gcp-auth!")
+
+    detail: str
+
+
+@app.get("/", summary="Root")
+async def root() -> Message:
+    """Root endpoint."""
+    return Message(detail=f"DT demo authentication service (version {version})")
+
+
+@app.get(
+    "/health",
+    summary="Health Check",
+    response_class=PlainTextResponse,
+    responses=plaintext_example("OK"),
+)
+async def health() -> str:
+    """Health check endpoint."""
+    return "OK"
+
+
+def main():
+    """Launch FastAPI dev server.
+
+    Supplies the entrypoint for the `auth` script in pyproject.toml.
+    """
+    # argv[1:] allows additional arguments to be passed to the FastAPI server.
+    sys.argv = f"fastapi dev {__file__}".split() + sys.argv[1:]
+
+    # Launch FastAPI dev server.
+    cli.main()
